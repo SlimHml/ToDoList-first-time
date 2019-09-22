a77e65e37c057d23690680d9b9a29f408506ec17
slim_hamila <slim.hamila.webd@gmail.com>
2019-09-22 23:27:55 +0200

    Todolist first time

---
+++ js.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+let li;
+
+function retourInput() {
+    return input.value.length;
+}
+
+function GetLiOnEnter(enter) {
+    if (enter.keyCode === 13) {
+        AddContentElement();
+    }
+}
+
+function createLi() {
+    li = document.createElement("li");
+}
+
+function createList() {
+    ul.append(li);
+}
+
+function addTextContent() {
+    li.append(document.createTextNode(input.value));
+    input.value = "";
+}
+
+function AddContentElement() {
+    if (retourInput() > 0) {
+        createLi();
+        addTextContent();
+        createList(); 
+    }
+}
+
+button.addEventListener("click", AddContentElement);
+button.addEventListener( "click" , AddContentElement);
